02fd6edd4ab9d35399d702acd323323f50cba8db
Alberto0150 <albertosanjaya2@gmail.com>
2021-05-29 00:13:46 +0700

    Fixing skill tab

--- application/views/mine.php
+++ application/views/mine.php
@@ -136,17 +136,8 @@ develop me especially in web development, IoT, and Data Management.</p>
 		  <p>Here is some of recapitulation of what i have done before</p>
         </div>
 
-        <div class="row">
-
-          <!-- <div class="col-lg-3 col-md-6">
-            <div class="count-box">
-              <i class="bi bi-emoji-smile"></i>
-              <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-              <p>Happy Clients</p>
-            </div>
-          </div> -->
-
-          <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
+        <div class="row" >
+          <div class="col-lg-3 col-md-6 mt-5 mt-md-0" style="flex-grow: 1;">
             <div class="count-box">
               <i class="bi bi-journal-richtext"></i>
               <span data-purecounter-start="0" data-purecounter-end="4" data-purecounter-duration="1" class="purecounter"></span>
@@ -154,7 +145,7 @@ develop me especially in web development, IoT, and Data Management.</p>
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-0" style="flex-grow: 1;">
             <div class="count-box">
               <i class="bi bi-headset"></i>
               <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1" class="purecounter"></span>
@@ -162,7 +153,7 @@ develop me especially in web development, IoT, and Data Management.</p>
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+          <div class="col-lg-3 col-md-6 mt-5 mt-lg-0" style="flex-grow: 1;">
             <div class="count-box">
               <i class="bi bi-award"></i>
               <span data-purecounter-start="0" data-purecounter-end="4" data-purecounter-duration="1" class="purecounter"></span>
@@ -181,48 +172,44 @@ develop me especially in web development, IoT, and Data Management.</p>
 
         <div class="section-title">
           <h2>Skills</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
         <div class="row skills-content">
 
-          <div class="col-lg-6">
+          <div class="col-lg-6-custom">
 
             <div class="progress">
-              <span class="skill">HTML <i class="val">Intermediate</i></span>
+              <span class="skill">HTML - Intermediate</span>
             </div>
-
             <div class="progress">
-              <span class="skill">CSS <i class="val">Intermediate</i></span>
+              <span class="skill">CSS - Intermediate</span>
             </div>
-
             <div class="progress">
-              <span class="skill">JavaScript <i class="val">Intermediate</i></span>
+              <span class="skill">JavaScript - Intermediate</span>
             </div>
           </div>
-
-          <div class="col-lg-6">
-
+          <div class="col-lg-6-custom">
             <div class="progress">
-              <span class="skill">PHP <i class="val">80%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
+              <span class="skill">PHP - Intermediate</span>
             </div>
 
             <div class="progress">
-              <span class="skill">WordPress/CMS <i class="val">90%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
+              <span class="skill">Python - Intermediate</span>
             </div>
 
             <div class="progress">
-              <span class="skill">Photoshop <i class="val">55%</i></span>
-              <div class="progress-bar-wrap">
-                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
+              <span class="skill">MySQL - Intermediate</span>
             </div>
+          </div>
+          <div class="col-lg-6-custom">
+            <div class="progress">
+              <span class="skill">Google Cloud Platform - Intermediate</span>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Arduino - Intermediate</span>
+            </div>
+
 
           </div>
 
